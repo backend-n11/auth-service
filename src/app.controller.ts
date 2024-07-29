9be48c31ac5f57aa5bc786e9c923f280724cbe7f
khamidullo <khkhamidullo@gmail.com>
2024-07-29 03:07:13 +0000

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,37 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
 import { ApiHeader, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { SkipThrottle } from "@nestjs/throttler";
 
 @SkipThrottle()
-@ApiTags('App')
+@ApiTags("App")
 @ApiHeader({
-  name: 'X-MyHeader',
-  description: 'Custom header',
+  name: "X-MyHeader",
+  description: "Custom header",
 })
 @Controller()
 export class AppController {
-
   @Get()
-  @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiResponse({
+    status: 201,
+    description: "The record has been successfully created.",
+  })
+  @ApiResponse({ status: 403, description: "Forbidden." })
   getRouteOne() {
-    return "salom dunyo!"
+    return "salom dunyo!";
   }
-
-
 
   @Get("test")
   getRouteTwo() {
-    for (let i = 0; i < 100000000000; i++) {
+    for (let i = 0; i < 100000000000; i++) {}
 
-    }
-
-    return "salom dunyo from test route!"
+    return "salom dunyo from test route!";
   }
 
   @SkipThrottle()
   @Get("skip")
   skipRouteOnRateLimit() {
-    return "ok "
+    return "ok ";
   }
 }
